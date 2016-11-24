24317deb3bd34defb6325ceddd43fd47ff27217f
Dan Sudenfield <dansudenfield@Dans-MacBook-Pro-2.local>
2016-11-24 15:51:46 -0500

    changes

--- bash_config_installer.sh
+++ bash_config_installer.sh
@@ -1,4 +1,7 @@
 cp .bash_profile ~/.bash_profile
 cp .bash_aliases ~/bash_aliases
-cp pre-push .git/hooks
+if [ ! -f .git/hooks/pre-push ]; then
+    cp pre-push .git/hooks
+    chmod 755 .git/hooks/pre-push
+fi
 source ~/.bash_profile
